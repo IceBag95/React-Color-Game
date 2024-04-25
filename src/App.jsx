f1351fb2318a92fc12a05b36f743ec90e71af56c
Panagiotis Pagonis <pan.m.pagonis@gmail.com>
2024-04-25 20:31:29 +0300

    Color Game stage 1

---
+++ src/App.jsx
@@ -0,0 +1,178 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [welcomeScreen, setWelcomeScreen] = useState(true) //fortwnetai MONO tin prwti fora, i sto refresh
+  const currentColor = HexColor()
+  const [myColor, setMyColor] = useState(`#${currentColor}`)
+  const [answers, setAnswers] = useState(Shuffle(createAnswers(currentColor)))
+  const [allAnswers, setallAnswers] = useState([]) //array of bool
+  const [gameOver, setGameOver] = useState(false) // na ftiaxnei to telos gia na deixnei to score
+  const [score, setScore] = useState(0)
+
+
+
+  function RandomHexValueInStr(){
+
+    const myNumberInt = Math.floor(Math.random()*15)
+
+    let myNumberStr;
+
+    switch (myNumberInt){
+      case 10:
+        myNumberStr = 'a'
+        break
+      case 11:
+        myNumberStr = 'b'
+        break
+      case 12:
+        myNumberStr = 'c'
+        break
+      case 13:
+        myNumberStr = 'd'
+        break
+      case 14:
+        myNumberStr = 'e'
+        break
+      case 15:
+        myNumberStr = 'f'
+        break
+      default:
+        myNumberStr = myNumberInt                     
+    }
+
+
+    return myNumberStr
+  }
+
+  function HexColor(){
+
+    //6 τιμές για color + #
+    let hexColorStr = ''
+    for(let i=0; i<6; i++){
+      hexColorStr += RandomHexValueInStr()
+      
+    }
+    return hexColorStr
+  }
+
+  function Shuffle(array){
+    for (let i = array.length -1 ; i>0; i--){
+      let randomIndex = Math.floor(Math.random() * array.length)
+      let temp = array[i] 
+      array[i] = array[randomIndex]
+      array[randomIndex] = temp
+    }
+
+    return array;
+  }
+
+
+  function createAnswers(color){
+    let wrongAnswer1, wrongAnswer2;
+    let strArray = color.split('')
+    while (true){
+      const strArrayShuffled = Shuffle(strArray)
+      if (strArrayShuffled.join('') !== color){
+        wrongAnswer1 = strArrayShuffled.join('')
+        break;
+      }
+    }
+
+    while (true){
+      const strArrayShuffled = Shuffle(strArray)
+      if (strArrayShuffled.join('') !== wrongAnswer1 && strArrayShuffled.join('') !== color){
+        wrongAnswer2 = strArrayShuffled.join('')
+        break;
+      }
+    }
+
+    return [`#${color}`,`#${wrongAnswer1}`,`#${wrongAnswer2}`]
+
+  }
+
+
+  function onButtonClick(event){
+    const ans = event.target.value
+    const updatedAllAnswers = [...allAnswers, (ans===myColor)]
+    if ((ans===myColor)){
+      setScore(s => s + 1)
+    }
+    setallAnswers(updatedAllAnswers) // theloume na deixnoume to score
+    if (updatedAllAnswers.length < 10){
+      const new_color = HexColor()
+      setMyColor(`#${new_color}`)
+      setAnswers(Shuffle(createAnswers(new_color)));
+    }
+    else{
+      setGameOver(g => !g )
+    }
+      
+  }
+
+  function RestartGame(){
+    const currentColor = HexColor()
+    setMyColor(`#${currentColor}`)
+    setAnswers(Shuffle(createAnswers(currentColor)))
+    setallAnswers([])
+    setGameOver(false)
+    setScore(0)
+  }
+
+
+  
+  
+  return(
+    <div id='app-container'>
+      <h1 id='app-title'>Guess The Color's Hex Value</h1>
+      {welcomeScreen ? 
+        <div id='welcome-screen'>
+          <input  className='buttons'
+                  type="button" 
+                  value='Start Game'
+                  onClick={() => setWelcomeScreen(w => !w)}/>
+        </div>
+      : !gameOver ? 
+        (<div id='game-container'>
+            <div  id='color-preview'
+                    style={{backgroundColor: myColor}}>
+                      {
+                        //myColor
+                      }
+            </div>
+            <div id='choice-container'>
+              {
+                answers.map((ans, index) => {
+                                              return <input type='button'
+                                                            value={ans}
+                                                            key={index}
+                                                            className='choices' 
+                                                            id={`choice_${index}`}
+                                                            onClick={(e) => onButtonClick(e)}/>
+                                            })
+              }
+            </div>
+
+            <input  className='choices'
+                    id='pass-button'
+                    type='button'
+                    value='Pass'
+                    onClick={(e) => onButtonClick(e)}/>
+        </div>)
+        :
+        (<div id='game-over-container'>
+          <h1>{score}/{allAnswers.length}</h1>
+          <input  className='buttons'
+                  type="button" 
+                  value={'Start Over'}
+                  onClick={RestartGame}/>
+        </div>)
+
+      }  
+
+    </div>
+  )
+}
+
+export default App
